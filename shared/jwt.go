b2996f876d10fbd2b77f7ad77cd1775d3138eec6
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2025-04-25 06:56:11 +0700

    [shared] add: add jwt for user functionality

--- shared/jwt.go
+++ shared/jwt.go
@@ -70,6 +70,62 @@ func ValidateJWTForService(tokenString string) (*ServiceCustomClaims, *jwt.Token
 	return claims, token, nil
 }
 
+func JWTUserMiddleware(c *fiber.Ctx) error {
+	jwtToken, err := GetTokenFromRequest(c)
+	if err != nil {
+		slog.Error("Error getting token from request", "err", err)
+		return fiber.NewError(fiber.StatusUnauthorized, err.Error())
+	}
+
+	token, err := ValidateJWTForUser(jwtToken)
+	if err != nil || !token.Valid {
+		slog.Error("Error validating token", "err", err)
+		return fiber.NewError(fiber.StatusUnauthorized, "Invalid token")
+	}
+
+	return c.Next()
+}
+
+func GenerateJWTForUser(userID string, expiry time.Time) (string, error) {
+	claims := jwt.RegisteredClaims{
+		Subject:   userID,
+		Issuer:    "topupstore-microservice",
+		ExpiresAt: jwt.NewNumericDate(expiry),
+		NotBefore: jwt.NewNumericDate(time.Now()),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+	}
+
+	secret := getSecretKey()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secret)
+}
+
+func ValidateJWTForUser(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Validate the algorithm
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		// Return the secret key for validation
+		return getSecretKey(), nil
+	})
+}
+
+func GetUserIdFromToken(c *fiber.Ctx) (string, error) {
+	token, err := GetTokenFromRequest(c)
+	if err != nil {
+		return "", fiber.NewError(fiber.StatusUnauthorized, err.Error())
+	}
+
+	claims, err := ValidateJWTForUser(token)
+	if err != nil {
+		return "", fiber.NewError(fiber.StatusUnauthorized, "Invalid token")
+	}
+
+	return claims.Claims.(jwt.RegisteredClaims).Subject, nil
+}
+
 func GetTokenFromRequest(c *fiber.Ctx) (string, error) {
 	// Extract JWT from the Authorization header
 	authHeader := c.Get("Authorization")
