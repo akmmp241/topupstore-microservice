38f495d48c913f04a1742431045d6d4d7ab9c860
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2025-04-25 06:55:32 +0700

    [auth-service] feat: implement login

--- auth_service/auth_service.go
+++ auth_service/auth_service.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/akmmp241/topupstore-microservice/shared"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 	"log/slog"
+	"strconv"
+	"time"
 )
 
 type AuthService struct {
@@ -39,7 +44,7 @@ func (s *AuthService) handleRegister(c *fiber.Ctx) error {
 		return shared.NewFailedValidationError(*registerRequest, err.(validator.ValidationErrors))
 	}
 
-	resp, err := CallUserService("/user-service/create", fiber.MethodPost, registerRequest)
+	resp, err := CallUserService("/users", fiber.MethodPost, registerRequest)
 	if err != nil {
 		slog.Error("Error occurred while calling user service", "err", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -64,6 +69,8 @@ func (s *AuthService) handleRegister(c *fiber.Ctx) error {
 		})
 	}
 
+	// TODO: seng message to kafka for email verification
+
 	// Handle user registration
 	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
 		"message": "User registered successfully",
@@ -73,6 +80,75 @@ func (s *AuthService) handleRegister(c *fiber.Ctx) error {
 }
 
 func (s *AuthService) Login(c *fiber.Ctx) error {
-	slog.Info("Logging in user")
-	return c.SendString("Login successfully")
+
+	loginRequest := &LoginRequest{}
+	err := c.BodyParser(loginRequest)
+	if err != nil {
+		slog.Error("Error occurred while parsing request body", "err", err)
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid request body")
+	}
+
+	err = s.Validator.Struct(loginRequest)
+	if err != nil && errors.As(err, &validator.ValidationErrors{}) {
+		return shared.NewFailedValidationError(*loginRequest, err.(validator.ValidationErrors))
+	}
+
+	url := fmt.Sprintf("/users?email=%s", loginRequest.Email)
+
+	resp, err := CallUserService(url, fiber.MethodGet, nil)
+	if err != nil || len(resp.Errs) > 0 {
+		slog.Error("Error occurred while calling user service", "errs", resp.Errs)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Internal Server Error",
+			"errors":  nil,
+		})
+	}
+
+	if resp.StatusCode != fiber.StatusOK {
+		slog.Error("User service returned non-200 status code", "code", resp.StatusCode)
+		return c.Status(resp.StatusCode).JSON(fiber.Map{
+			"message": string(resp.Body),
+			"errors":  nil,
+		})
+	}
+
+	globalResponse := &GlobalServiceResponse{}
+	err = json.Unmarshal(resp.Body, globalResponse)
+	if err != nil {
+		slog.Error("Error occurred while unmarshalling response body", "err", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Internal Server Error",
+			"errors":  nil,
+		})
+	}
+
+	userResponse := globalResponse.Data.(GetUserResponse)
+
+	err = bcrypt.CompareHashAndPassword([]byte(userResponse.Password), []byte(loginRequest.Password))
+	if err != nil {
+		slog.Error("Error occurred while comparing passwords", "err", err)
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Invalid credentials",
+		})
+	}
+
+	// Generate JWT token
+	expiry := time.Now().Add(time.Hour * 24)
+	userId := strconv.Itoa(userResponse.Id)
+	accessToken, err := shared.GenerateJWTForUser(userId, expiry)
+	if err != nil {
+		slog.Error("Error occurred while generating JWT token", "err", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Internal Server Error",
+			"errors":  nil,
+		})
+	}
+
+	// TODO: Send message to Kafka for email login
+
+	return c.JSON(fiber.Map{
+		"message": "Login successful",
+		"data":    fiber.Map{"access_token": accessToken},
+		"errors":  nil,
+	})
 }
--- auth_service/dto.go
+++ auth_service/dto.go
@@ -7,3 +7,22 @@ type RegisterRequest struct {
 	PhoneNumber          string `json:"phone_number" validate:"required,min=10,max=15"`
 	PasswordConfirmation string `json:"password_confirmation" validate:"required,eqfield=Password"`
 }
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8,max=255"`
+}
+
+type GlobalServiceResponse struct {
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+	Errors  any    `json:"errors"`
+}
+
+type GetUserResponse struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	PhoneNumber string `json:"phone_number"`
+}
