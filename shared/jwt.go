c7e538b8c459d0db2d2d91c7cc54703304c9d4b0
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2025-04-24 08:12:37 +0700

    feat: add helper function for jwt functionality in services communication

--- shared/jwt.go
+++ shared/jwt.go
@@ -1,22 +1,36 @@
 package shared
 
 import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
 	"os"
 	"time"
 )
 
 var (
-	secretKey []byte // Replace with your actual secret key
+	serviceSecretKey []byte // Replace with your actual secret key
+	secretKey        []byte
 )
 
-type CustomClaims struct {
+type ServiceCustomClaims struct {
 	Service string `json:"service"`
 	jwt.RegisteredClaims
 }
 
+func JWTServiceMiddleware(c *fiber.Ctx) error {
+	jwtToken := GetTokenFromRequest(c)
+
+	_, token, err := ValidateJWTForService(jwtToken)
+	if err != nil || !token.Valid {
+		return fiber.NewError(fiber.StatusUnauthorized, "Invalid token")
+	}
+
+	return c.Next()
+}
+
 func GenerateJWTForService(serviceName string) (string, error) {
-	claims := CustomClaims{
+	claims := ServiceCustomClaims{
 		Service: serviceName,
 		RegisteredClaims: jwt.RegisteredClaims{
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(15 * time.Minute)),
@@ -25,10 +39,45 @@ func GenerateJWTForService(serviceName string) (string, error) {
 		},
 	}
 
-	secret := getSecretKey()
+	secret := getServiceSecretKey()
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(secret)
+}
+
+func ValidateJWTForService(tokenString string) (*ServiceCustomClaims, *jwt.Token, error) {
+	claims := &ServiceCustomClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		// Validate the algorithm
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		// Return the secret key for validation
+		return getServiceSecretKey(), nil
+	})
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return claims, token, nil
+}
+
+func GetTokenFromRequest(c *fiber.Ctx) string {
+	jwtToken := c.Get("Authorization")
+
+	return jwtToken
+}
+
+func getServiceSecretKey() []byte {
+	if serviceSecretKey != nil {
+		return serviceSecretKey
+	}
+
+	key := os.Getenv("SERVICE_JWT_SECRET_KEY")
+	serviceSecretKey = []byte(key)
+
+	return serviceSecretKey
 }
 
 func getSecretKey() []byte {
@@ -36,7 +85,7 @@ func getSecretKey() []byte {
 		return secretKey
 	}
 
-	key := os.Getenv("SERVICE_JWT_SECRET_KEY")
+	key := os.Getenv("USER_JWT_SECRET_KEY")
 	secretKey = []byte(key)
 
 	return secretKey
