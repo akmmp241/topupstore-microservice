460942837a53d7df00282fc3ea784f1050ff10d3
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2025-04-28 16:32:52 +0700

    [user-service] feat: implement verifying user

--- user_service/dto.go
+++ user_service/dto.go
@@ -1,9 +1,10 @@
 package main
 
 type RegisterRequest struct {
-	Email                string `json:"email" validate:"required,email"`
-	Password             string `json:"password" validate:"required,min=8,max=255"`
-	Name                 string `json:"name" validate:"required,min=2,max=255"`
-	PhoneNumber          string `json:"phone_number" validate:"required,min=10,max=15"`
-	PasswordConfirmation string `json:"password_confirmation" validate:"required,eqfield=Password"`
+	Email                  string `json:"email" validate:"required,email"`
+	Password               string `json:"password" validate:"required,min=8,max=255"`
+	Name                   string `json:"name" validate:"required,min=2,max=255"`
+	PhoneNumber            string `json:"phone_number" validate:"required,min=10,max=15"`
+	PasswordConfirmation   string `json:"password_confirmation" validate:"required,eqfield=Password"`
+	EmailVerificationToken string `json:"email_verification_token" validate:"required"`
 }
--- user_service/user_service.go
+++ user_service/user_service.go
@@ -12,6 +12,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 	"strings"
+	"time"
 )
 
 type UserService struct {
@@ -31,6 +32,7 @@ func (s *UserService) RegisterRoutes(router fiber.Router) {
 	internalAPI.Get("/", s.handleGetUser)
 	internalAPI.Put("/:id", s.handleUpdateUser)
 	internalAPI.Delete("/:id", s.handleDeleteUser)
+	internalAPI.Patch("/verify/:token", s.handleVerifyEmail)
 
 	router.Get("/me", s.handleGetUser)
 }
@@ -60,8 +62,8 @@ func (s *UserService) handleCreateUser(c *fiber.Ctx) error {
 	}
 	defer shared.CommitOrRollback(tx, err)
 
-	result, err := tx.ExecContext(s.Ctx, "INSERT INTO users (id, name, email, password, phone_number) VALUES (NULL, ?, ?, ?, ?)",
-		registerRequest.Name, registerRequest.Email, registerRequest.Password, registerRequest.PhoneNumber)
+	result, err := tx.ExecContext(s.Ctx, "INSERT INTO users (id, name, email, password,phone_number, email_verification_token) VALUES (NULL, ?, ?, ?, ?, ?)",
+		registerRequest.Name, registerRequest.Email, registerRequest.Password, registerRequest.PhoneNumber, registerRequest.EmailVerificationToken)
 	if err != nil {
 		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
 			slog.Error("MySQL error occurred", "code", mysqlErr.Number, "message", mysqlErr.Message)
@@ -154,4 +156,56 @@ func (s *UserService) handleUpdateUser(c *fiber.Ctx) error {
 func (s *UserService) handleDeleteUser(c *fiber.Ctx) error {
 	// Handle deleting user
 	return c.SendString("User deleted successfully")
+}
+
+func (s *UserService) handleVerifyEmail(c *fiber.Ctx) error {
+	token := c.Params("token")
+	if token == "" {
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid token")
+	}
+
+	query := "SELECT id, name, email, password, phone_number, email_verification_token, email_verified_at, created_at, updated_at FROM users WHERE email_verification_token = ?"
+
+	rows, err := s.DB.QueryContext(s.Ctx, query, token)
+	if err != nil {
+		slog.Debug("Error occurred while querying user", "err", err)
+		return err
+	}
+	defer rows.Close()
+
+	var user User
+	if !rows.Next() {
+		return fiber.NewError(fiber.StatusNotFound, "User not found")
+	}
+
+	var emailVerifiedAt sql.NullTime
+	err = rows.Scan(&user.Id, &user.Name, &user.Email, &user.Password, &user.PhoneNumber, &user.EmailVerificationToken, &emailVerifiedAt, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		slog.Debug("Error occurred while scanning user", "err", err)
+		return err
+	}
+
+	if emailVerifiedAt.Valid {
+		user.EmailVerifiedAt = emailVerifiedAt.Time
+	}
+
+	if user.EmailVerificationToken == token {
+		// Update the email verification token and set email verified at
+		user.EmailVerificationToken = ""
+		user.EmailVerifiedAt = time.Now()
+
+		_, err = s.DB.ExecContext(s.Ctx, "UPDATE users SET email_verification_token = ?, email_verified_at = ? WHERE id = ?", user.EmailVerificationToken, user.EmailVerifiedAt, user.Id)
+		if err != nil {
+			slog.Debug("Error occurred while updating user", "err", err)
+			return err
+		}
+	} else {
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid token")
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "Email verified successfully",
+		"data":    nil,
+		"errors":  nil,
+	})
 }
