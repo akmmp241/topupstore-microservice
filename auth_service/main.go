908b3b58bf812fc7d122c7a9ef5dfa0cd76ac8da
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2025-11-06 16:59:42 +0700

    [auth-service] refactor: standardize event payloads, centralize Kafka topic usage

--- auth_service/main.go
+++ auth_service/main.go
@@ -6,15 +6,7 @@ import (
 )
 
 func main() {
-	kafkaHost := os.Getenv("KAFKA_HOST")
-	kafkaPort := os.Getenv("KAFKA_PORT")
-
-	bootstrapServer := kafkaHost + ":" + kafkaPort
-
-	slog.Info("Starting Kafka producer", "bootstrap-server", bootstrapServer)
-	producer := NewKafkaProducer(bootstrapServer)
-
 	port := os.Getenv("AUTH_SERVICE_PORT")
 	slog.Info("Starting HTTP server on port:", "port", port)
-	(NewAppServer(producer)).Run(port)
+	(NewAppServer()).Run(port)
 }
